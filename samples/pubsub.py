3674558ebcd6504d22fcd657677568f403a5492b
jcwon989 <jcwon@pinotlab.com>
2024-01-18 22:59:14 +0900

    aaaa

--- samples/pubsub.py
+++ samples/pubsub.py
@@ -8,7 +8,7 @@ import threading
 import time
 import json
 from utils.command_line_utils import CommandLineUtils
-import socket
+import netifaces as ni
 
 # This sample uses the Message Broker for AWS IoT to send and receive messages
 # through an MQTT connection. On startup, the device connects to the server,
@@ -73,12 +73,10 @@ def on_connection_failure(connection, callback_data):
 def on_connection_closed(connection, callback_data):
     print("Connection closed")
 
-def get_ip_address():
+def get_wlan0_ip_address():
     try:
-        # 호스트 이름을 가져와서 IP 주소로 변환
-        hostname = socket.gethostname()
-        ip_address = socket.gethostbyname(hostname)
-        return ip_address
+        ip_info = ni.ifaddresses('wlan0')[ni.AF_INET][0]
+        return ip_info['addr']
     except:
         return "Unable to get IP Address"
 
@@ -132,7 +130,7 @@ if __name__ == '__main__':
     print("Subscribed with {}".format(str(subscribe_result['qos'])))
 
     # Start Message
-    ip_address = get_ip_address()
+    ip_address = get_wlan0_ip_address()
     message = "{} [{}]".format("boot complete", ip_address)
     print("Publishing message to topic '{}': {}".format(message_topic, message))
     message_json = json.dumps(message)

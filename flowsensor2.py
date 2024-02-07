48feba2ce01ee3442d8beb0b2b46ec8a2fb25709
jcwon989 <jcwon@pinotlab.com>
2024-02-07 18:38:36 +0900

    flow

--- flowsensor2.py
+++ flowsensor2.py
@@ -6,7 +6,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(sensor_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 pulse_count = 0
-calibration_factor = 4.5
+calibration_factor = 5.5
 flow_rate = 0.0
 flow_millilitres = 0
 total_millilitres = 0
@@ -23,12 +23,12 @@ try:
     while True:
         if time.time() - old_time > 1:
             flow_rate = (pulse_count / calibration_factor) / (time.time() - old_time) * 60
-            flow_millilitres = (flow_rate / 60) * 1000
+            flow_millilitres = (flow_rate / 60) * 16700
             total_millilitres += flow_millilitres
             
-            print(f"Flow rate: {flow_rate:.2f} L/min")
-            print(f"Output Liquid Quantity: {total_millilitres:.2f} mL")
-            print(f"{total_millilitres/1000:.2f} L")
+            print(f"Flow rate: {flow_rate:.2f} mL/min")
+            # print(f"Output Liquid Quantity: {total_millilitres:.2f} mL")
+            print(f"{total_millilitres/1000:.2f} mL")
             
             pulse_count = 0
             old_time = time.time()
